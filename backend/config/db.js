0314476fa5ba400b273a99bcdf09758656994c90
Arnav aggarwal <arnavaggarwal999000@gmail.com>
2021-09-01 13:52:46 +0530

    deploying it

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,22 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose"
+
+const URL = process.env.MONGO_URI || "mongodb://arnav_sys:aa1aa2sam3@cluster0-shard-00-00.iqnbd.mongodb.net:27017,cluster0-shard-00-01.iqnbd.mongodb.net:27017,cluster0-shard-00-02.iqnbd.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-fq1v00-shard-0&authSource=admin&retryWrites=true&w=majority"
+
+
 
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-    })
-
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
-  } catch (error) {
-    console.error(`Error: ${error.message}`.red.underline.bold)
-    process.exit(1)
-  }
+    try {
+        const conn = await mongoose.connect(URL,{
+            useUnifiedTopology:true,
+            useNewUrlParser:true,
+            useCreateIndex:true
+        })
+        console.log(URL)
+        console.log("MongoDB Connected: " + conn.connection.host)
+    }catch(error){
+        console.log(error.message)
+        process.exit(1)
+    }
 }
 
 export default connectDB
